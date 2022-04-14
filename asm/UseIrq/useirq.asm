f317730f75c463d7a6a48b9d1e19e654412c3509
Brian G <justlostintime@gmail.com>
2022-04-14 15:40:57 -0700

    Add Timer deviced base upon the cpu cycle clock, bug fixes, enhance profiling [6502] * BUG: Bug when there were more than 10 devices attached to a cpu * NEW: Add device interface for Getting the cpu object, and executing a cpuinit for each device if present * OPT: Profiling screen can now be exited and restarted without loosing profile information * OPT: Enlarged Address symbol names for profiles to display entire name * OPT: Speed up the tty device interface a little * NEW: Added 6502 asm program/makefile to demonstrate and test the different timers and NMI * NEW: Updated provided config.inc 6502 asm file with timer device entry points See ~/6502/asm/useirq directory

--- asm/UseIrq/useirq.asm
+++ asm/UseIrq/useirq.asm
@@ -58,14 +58,47 @@ CodeStart jsr puts
 		sta	NMIvec+1
           cli
           jmp MONITOR
+          
+SaveReg     db 0,0,0
 
-ServiceIrq  jsr puts
-            db "IRQ Recieved"
+ServiceIrq  sta SaveReg
+	    sty SaveReg+1
+	    stx SaveReg+2
+	    
+	    lda SysTime                 ;Check if this created the IRQ
+	    beq cycletimer
+	    jsr puts                    ; the timer that uses a real system timer
+	    db "IRQ Real Timer"
+	    db CR,LF,0
+	    lda #0                      ; Zero the pending and reset timer
+	    sta SysTime_Ack             ; Write anything to reset the timer
+	    beq ExitIrq
+
+cycletimer  lda CycleTime               ; Check timer based upon clock cycles ticked
+	    beq NotTimer
+	    jsr puts                    ; the timer that uses a Cycle Clock as a timer source
+	    db "IRQ Cycle Timer"
+	    db CR,LF,0
+	    lda #0                      ; Zero the pending and reset timer
+	    sta CycleTime_Ack           ; Write anything to reset the timer
+	    beq ExitIrq
+	    
+NotTimer    jsr puts                    ; we have some unknown irq happening
+            db "IRQ UnKnown"
             db CR,LF,0
-            rti
+	    lda #0
+            beq ExitIrq
             
-ServiceNmi  jsr puts
+ServiceNmi  sta SaveReg
+	    sty SaveReg+1
+	    stx SaveReg+2
+	    
+            jsr puts
             db "NMI Recieved"
             db CR,LF,0
+            
+ExitIrq	    lda SaveReg
+	    ldy SaveReg+1
+	    ldx SaveReg+2
             rti
 
